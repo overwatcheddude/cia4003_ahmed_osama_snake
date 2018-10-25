17bd85ad3b8186c800ac8126cc1458f7d536b98f
overwatcheddude <overwatcheddude@gmail.com>
2018-10-25 15:58:41 +0400

    first commit

---
+++ app/src/main/java/com/example/user/snakegame/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.user.snakegame;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.graphics.Point;
+import android.os.Bundle;
+import android.view.Display;
+
+public class MainActivity extends AppCompatActivity
+{
+    // Declare an instance of SnakeEngine
+    SnakeEngine snakeEngine;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        // Get the pixel dimensions of the screen
+        Display display = getWindowManager().getDefaultDisplay();
+
+        // Initialize the result into a Point object
+        Point size = new Point();
+        display.getSize(size);
+
+        // Create a new instance of the SnakeEngine class
+        snakeEngine = new SnakeEngine(this, size);
+
+        // Make snakeEngine the view of the Activity
+        setContentView(snakeEngine);
+    }
+
+    // Start the thread in snakeEngine
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        snakeEngine.resume();
+    }
+    // Stop the thread in snakeEngine
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        snakeEngine.pause();
+    }
+}
