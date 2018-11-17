b6dd83d65c5ffa8af3596445264d5915b771d153
overwatcheddude <overwatcheddude@gmail.com>
2018-11-17 12:51:47 +0400

    Added gestures

--- app/src/main/java/com/example/user/snakegame/GameActivity.java
+++ app/src/main/java/com/example/user/snakegame/GameActivity.java
@@ -15,7 +15,7 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
 {
     // Declare an instance of SnakeEngine
     SnakeEngine snakeEngine;
-    private GestureDetector myGestureDetector;
+    public GestureDetector myGestureDetector;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -54,9 +54,80 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
     }
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
+    public boolean onTouch(View v, MotionEvent event)
+    {
         myGestureDetector.onTouchEvent(event);
+        return true;
+    }
 
+    @Override
+    public boolean onDown(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public void onShowPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onSingleTapUp(MotionEvent e) {
+        return false;
+    }
+
+    @Override
+    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+        return false;
+    }
+
+    @Override
+    public void onLongPress(MotionEvent e) {
+
+    }
+
+    @Override
+    public boolean onFling(MotionEvent downEvent, MotionEvent moveEvent, float velocityX, float velocityY) {
+        float diffY = moveEvent.getY() - downEvent.getY();
+        float diffX = moveEvent.getX() - downEvent.getX();
+        //Which was greater? Movement across Y or X?
+        if (Math.abs(diffX) > Math.abs(diffY))
+        {
+            //Right or left swipe.
+            if (Math.abs(diffX) > 25 && Math.abs(velocityX) > 25)
+            {
+                if (diffX > 0)
+                {
+                    //Swipe is right.
+                    snakeEngine.movement = SnakeEngine.Moving.RIGHT;
+                }
+                else
+                {
+                    //Swipe is left.
+                    snakeEngine.movement = SnakeEngine.Moving.LEFT;
+                }
+            }
+        }
+        else
+        {
+            //Up or down swipe.
+            if (Math.abs(diffY) > 25 && Math.abs(velocityY) > 25)
+            {
+                if (diffY > 0)
+                {
+                    //Swipe is down.
+                    snakeEngine.movement = SnakeEngine.Moving.DOWN;
+                }
+                else
+                {
+                    //Swipe is up.
+                    snakeEngine.movement = SnakeEngine.Moving.UP;
+                }
+            }
+        }
+        return true;
+    }
+}
+/*
         switch (event.getAction() & MotionEvent.ACTION_MASK)
         {
             case MotionEvent.ACTION_UP:
@@ -91,36 +162,4 @@ public class GameActivity extends AppCompatActivity implements View.OnTouchListe
                     }
                 }
         }
-        return true;
-    }
-
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        return false;
-    }
-
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        return false;
-    }
-}
+ */
