1c4d1f214fad096c2034322011aff8e162b9db51
overwatcheddude <overwatcheddude@gmail.com>
2018-11-17 18:01:15 +0400

    User can now set avatar by gallery or camera

--- app/src/main/java/com/example/user/snakegame/profile.java
+++ app/src/main/java/com/example/user/snakegame/profile.java
@@ -1,8 +1,10 @@
 package com.example.user.snakegame;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -23,10 +25,14 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 
 public class profile extends AppCompatActivity {
+    private static final int CAMERA_CAPTURE_IMAGE_REQUEST_CODE = 100;
     private General general;
     private TextView tvUploadRate;
+    private ImageView ivAvatar;
+    private String setOption;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +41,7 @@ public class profile extends AppCompatActivity {
 
         tvUploadRate = findViewById(R.id.tvUploadRate);
         general = new General(getApplicationContext());
+        ivAvatar = findViewById(R.id.ivAvatar);
     }
 
     public void BackToMenu(View v)
@@ -48,8 +55,6 @@ public class profile extends AppCompatActivity {
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
         FirebaseStorage storage = FirebaseStorage.getInstance();
         StorageReference storageRef = storage.getReference();
-
-        ImageView ivAvatar = findViewById(R.id.ivAvatar);
 
         ivAvatar.setDrawingCacheEnabled(true);
         ivAvatar.buildDrawingCache();
@@ -92,5 +97,61 @@ public class profile extends AppCompatActivity {
             }
         });
 
+    }
+
+    public  void setImage(View v)
+    {
+        Intent i = new Intent();
+        switch (v.getId())
+        {
+
+            case R.id.btnSetByCamera:
+                i = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+                setOption = "Camera";
+                break;
+            case R.id.btnSetByGallery:
+                i = new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                setOption = "Gallery";
+                break;
+            default: general.DisplayMessage("Unknown set button clicked.");
+        }
+        startActivityForResult(i, CAMERA_CAPTURE_IMAGE_REQUEST_CODE);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        // if the result is capturing Image
+        if (requestCode == CAMERA_CAPTURE_IMAGE_REQUEST_CODE)
+        {
+            if (resultCode == RESULT_OK)
+            {
+                if (setOption.equals("Camera"))
+                {
+                    Bitmap photo = (Bitmap) data.getExtras().get("data");
+                    ivAvatar.setImageBitmap(photo);
+                }
+                if (setOption.equals("Gallery"))
+                {
+                    Uri imageUri = data.getData();
+                    Bitmap bitmap = null;
+                    try
+                    {
+                        bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(), imageUri);
+                    } catch (IOException e)
+                    {
+                        e.printStackTrace();
+                    }
+                    ivAvatar.setImageBitmap(bitmap);
+                }
+            }
+            else if (resultCode == RESULT_CANCELED)
+            {
+                general.DisplayMessage("User cancelled image capture");
+            }
+            else
+            {
+                general.DisplayMessage("Sorry! Failed to capture image");
+            }
+        }
     }
 }
