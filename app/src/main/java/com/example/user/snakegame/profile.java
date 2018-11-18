491b43276b8e2d567cf65c52079ee8a17ec23ad3
overwatcheddude <overwatcheddude@gmail.com>
2018-11-18 18:12:23 +0400

    Added progress bar to glide image load in profile class

--- app/src/main/java/com/example/user/snakegame/profile.java
+++ app/src/main/java/com/example/user/snakegame/profile.java
@@ -3,6 +3,7 @@ package com.example.user.snakegame;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
@@ -11,11 +12,16 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.DataSource;
 import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.engine.GlideException;
+import com.bumptech.glide.request.RequestListener;
 import com.bumptech.glide.request.RequestOptions;
+import com.bumptech.glide.request.target.Target;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
@@ -55,6 +61,8 @@ public class profile extends AppCompatActivity {
 
     private void loadImageFromDB()
     {
+        final ProgressBar progressBar = findViewById(R.id.progressBar);
+
         // Reference to an image file in Cloud Storage
         StorageReference pathRef = storageRef.child("images/" + user.getEmail());
 
@@ -67,6 +75,19 @@ public class profile extends AppCompatActivity {
         Glide.with(this)
                 .applyDefaultRequestOptions(options)
                 .load(pathRef)
+                .listener(new RequestListener<Drawable>() {
+                    @Override
+                    public boolean onLoadFailed(GlideException e, Object model, Target<Drawable> target, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+
+                    @Override
+                    public boolean onResourceReady(Drawable resource, Object model, Target<Drawable> target, DataSource dataSource, boolean isFirstResource) {
+                        progressBar.setVisibility(View.GONE);
+                        return false;
+                    }
+                })
                 .into(ivAvatar);
         Log.i("", "Glide called!");
     }
